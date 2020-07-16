23c0ad123dd968f5a4ea79e82fbea8cb787d0fb4
Greg Maxey <gregorymaxey@yahoo.com>
2020-07-16 20:55:20 +0100

    Add shell step

--- Command line credentials.groovy
+++ Command line credentials.groovy
@@ -6,8 +6,14 @@ When it is necessary to supply credentials in a Command block in Flow, there are
 1. Include the credentials values in the command. This exposes the credentials in the step or task definition and the job log file.
 2. Use $[/javascript api.getFullCredential(...).password] to retrieve the credentials at runtime. In this case, the credentials will not be exposed in the step definition, but they will be exposed in the job log file.
 3. Wrap the command line call in an ec-perl or ec-groovy script. This will prevent the credentials from being exposed in both the definition and the job log file.
+5. Use environment variables and $(ectool getFullCredential ...) to avoid exposing the credentials in log files or the command definition.
 
-This example illustrates how wrap a command line call in ec-groovy. It creates a secure credential and a procedure with a step that uses that credential.
+This example illustrates how wrap a command line call in ec-groovy and also how to run a shell command. It creates a secure credential and a procedure with a step that uses that credential.
+
+Note that credentials stored in a different project can be used, but must be fully qualified:
+
+/projects/My Cred Project/credentials/My Cred
+/projects/$[/plugins/EC-Jenkins]/credential/My Jenkins Creds
 
 
 */
@@ -17,7 +23,7 @@ def CredentialName = "myuser"
 project "Credentials",{
 	credential CredentialName, userName: "myuser", password: "mypass"
 	procedure 'Command line credentials', {
-		step 'Use credentials in a shell', {
+		step 'Use credentials in a ec-groovy', {
 			description = 'ec-groovy wrapped command line that uses credential values'
 			shell = 'ec-groovy'
 			command = """\
@@ -30,6 +36,17 @@ project "Credentials",{
 					This example uses 'echo' to purposefully show the credentials as proof that this mechanism works. When using this approach, make sure not to echo the secret part of the credential.
 				*/
 				println "echo Username: \$userName, Password: \$password".execute().text
+			""".stripIndent()
+			attachCredential {
+				credentialName = "/projects/$projectName/credentials/$CredentialName"
+			}
+		}
+		step 'Use credentals in a shell',{
+			command = """\
+				USER=\$(ectool getFullCredential "${CredentialName}" --value userName)
+				PASS=\$(ectool getFullCredential "${CredentialName}" --value password)
+				echo \$USER
+				echo \$PASS > /dev/null
 			""".stripIndent()
 			attachCredential {
 				credentialName = "/projects/$projectName/credentials/$CredentialName"
