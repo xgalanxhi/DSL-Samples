f40be8571a2336c699758ccc079bb8ac4bf027fe
Greg Maxey <gregorymaxey@yahoo.com>
2020-07-20 20:15:43 +0100

    Add CR and job links

--- ServiceNowPollingEcGroovy.groovy
+++ ServiceNowPollingEcGroovy.groovy
@@ -2,10 +2,9 @@
 
 CloudBees CD DSL: Poll ServiceNow using ec-groovy
 
+This procedure can be used for pipeline gates.
+
 TODO
-- Add polling job link
-- Add ServiceNow record link
-	https://xxxx.service-now.com/nav_to.do?uri=change_request.do?sys_id=ac46ceccdb6cdc1038c0e3364896198f
 
 
 */
@@ -35,6 +34,9 @@ project "ServiceNow",{
 				def RunResponse = ef.runProcedure procedureName: 'GetRecord', projectName: '/plugins/EC-ServiceNow/project', 		actualParameters: params
 
 				def JobId = RunResponse.jobId
+				ef.setProperty propertyName: "/myJob/report-urls/Get Status Job", value: "link/jobDetails/jobs/${JobId}"
+				
+				
 				// Not available in ec-groovy
 				//ef.waitForJob jobId: JobId
 				sleep 5000 // 5 seconds
@@ -42,6 +44,8 @@ project "ServiceNow",{
 				def SN_ResponseJson = ef.getProperty(propertyName: "/myJob/ResponseContent", jobId: JobId).property.value
 				def Slurper = new JsonSlurper()
 				def ApprovalStatus = Slurper.parseText(SN_ResponseJson)[0].approval
+				def SysId = Slurper.parseText(SN_ResponseJson)[0].sys_id
+				ef.setProperty propertyName: "/myJob/report-urls/ServiceNow Record: $[RecordID]", value: "\$[/plugins/EC-ServiceNow/project/ServiceNow_cfgs/\$[Configuration]/host]/nav_to.do?uri=change_request.do?sys_id=${SysId}"
 				println "Approval status: $ApprovalStatus"
 				return ApprovalStatus
 			}
