fe84c598747df3df71ba7483ba881c9c6e27f6f1
Greg Maxey <gregorymaxey@yahoo.com>
2020-11-10 16:03:19 +0000

    Move share values out to variables, fix problem with regex

--- PostpKeyValuePair.groovy
+++ PostpKeyValuePair.groovy
@@ -1,67 +1,53 @@
 /*
 
-CloudBees CD DSL: Use postp to store command responses to properties
+CloudBees CD DSL: Use postprocessor (postp) to store command responses to properties
 
-Provided here are a procedure and a pipeline that show how to use the postp capabilities of CloudBees CD
+Creates a procedure and a pipeline that illustrate how to use the postprocessor capabilities of CloudBees CD
 to save command-line outputs to properties. The postprocessor looks for key value pairs that it stores as
-properties. These key value pairs are identified by find the = and : symbols.
+properties. These key value pairs are identified by finding = and : separator symbols.
 
-For the procedure, the properties are stored the job property sheet.
-For the pipeline, the properties are stored to the stage summary.
+For the procedure, the properties are stored the job property sheet
+For the pipeline, the properties are stored to the stage summary
 
 */
 
+def KeyValuePP = '''\
+	@::gMatchers = (
+		{
+			id      => "KeyValue",
+			pattern => q{([^ ]+)\\s*[:=]\\s*(.*)},
+			action  => q{
+				# /javascript returns "myStageRuntime/ec_summary" if run from a pipeline, "myJob" otherwise
+				setProperty("/$[/javascript getProperty("/myPipelineRuntime")?"myStageRuntime/ec_summary":"myJob"]/" . $1, $2)
+			},
+		}
+	);
+'''.stripIndent()
+
+def CommandBlock = '''\
+	cat << EOF
+	testresult: PASS
+	coverage = 95%
+	testname : Total System Integration
+	EOF
+'''.stripIndent()
+
 project "Postp",{
+	property "KeyValuePP", value: KeyValuePP
 	procedure "Use Postp",{
 		step "Grab properties from response",{
 			description = "Run command and scrape key-value pairs as properties"
-			command = '''\
-				cat << EOF
-				line1 output
-				mycount: 12345
-				anotherval: abcdef
-				Some text and myval=1.1.1.1
-				EOF
-			'''.stripIndent()
-			postProcessor = 'postp --loadProperty /myStep/KeyValue'
-			property "KeyValue", value: '''\
-				@::gMatchers = (
-					{
-						id      => "KeyValue",
-						pattern => q{([ ]+)\\s*[:=]\\s*([^ ]+)},
-						action  => q{
-							setProperty("/myJob/" . $1, $2)
-						},
-					}
-				);
-			'''.stripIndent()
+			command = CommandBlock
+			postProcessor = 'postp --loadProperty "/myProject/KeyValuePP"'
 		}
 	}
 	pipeline "Use Postp",{
 		stage "Stage 1",{
 			task "Grab properties from response",{
-				actualParameter = [ commandToRun : '''\
-						cat << EOF
-						line1 output
-						mycount: 12345
-						anotherval: abcdef
-						Some text and myval=1.1.1.1
-						EOF
-					'''.stripIndent(),
-					postp : 'postp --loadProperty /myTask/KeyValue'
+				actualParameter = [
+					commandToRun : CommandBlock,
+					postp : 'postp --loadProperty "/projects/$[/myPipelineRuntime/projectName]/KeyValuePP"'
 				]
-				property "KeyValue", value: '''\
-					@::gMatchers = (
-						{
-							id      => "KeyValue",
-						pattern => q{([ ]+)\\s*[:=]\\s*([^ ]+)},
-							action  => q{
-								setProperty("/myStageRuntime/ec_summary/" . $1, $2)
-							},
-						}
-					);
-				'''.stripIndent()
-				
 				subpluginKey = 'EC-Core'
 				subprocedure = 'RunCommand'
 				taskType = 'COMMAND'
