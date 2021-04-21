a240468e47945b73c5d94d657e2ae2f07c59dad6
Greg Maxey <gregorymaxey@yahoo.com>
2021-04-21 23:28:29 +0100

    Initial

---
+++ EnforceComment.groovy
@@ -0,0 +1,27 @@
+/*
+
+CloudBees SDA CD/RO DSL: Fail pipeline if a comment is not provided in the Manual task
+
+*/
+
+project "DSL-Samples",{
+	pipeline 'Enforce Comment', {
+		stage 'Stage 1', {
+			task 'Manual task', {
+				instruction='Comment field should be filled in, otherwise the pipeline will fail.'
+				notificationTemplate = 'ec_default_pipeline_manual_task_notification_template'
+				taskType = 'MANUAL'
+				approver = [
+					'Everyone',
+				]
+			}
+			gate 'POST', {
+				task 'Verify Comment Exists', {
+					gateCondition = '$[/javascript myPipelineRuntime.stages["Stage 1"].tasks["Manual Task"].evidence.length]'
+					gateType = 'POST'
+					taskType = 'CONDITIONAL'
+				}
+			}
+		}
+	}
+}
