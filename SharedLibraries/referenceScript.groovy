17ea0048738a5f142746c7441397ea689d41e79d
Greg Maxey <gregorymaxey@yahoo.com>
2021-10-04 19:45:19 +0100

    Initial

---
+++ SharedLibraries/referenceScript.groovy
@@ -0,0 +1,16 @@
+import groovy.transform.BaseScript
+import com.electriccloud.commander.dsl.sample.DslBaseScript
+
+// DslBaseScript encapsulates the magic for invoking dsl scripts
+@BaseScript DslBaseScript baseScript
+
+// Examples for using shared scripts and code
+
+// 1. Evaluate basic dsl script
+evalDslScript 'scripts/project.dsl'
+
+// 2. Create a pipeline based on a template
+createPipelineFromTemplate 'Pipeline project', 'Sample pipeline'
+
+// 3. Add a task based on a template to existing pipeline
+addPreGateApprovalTask 'Pipeline project', 'Sample pipeline', 'Stage 1'
---
+++ SharedLibraries/sharedFiles/scripts/project.dsl
@@ -0,0 +1,1 @@
+project "My new project"
