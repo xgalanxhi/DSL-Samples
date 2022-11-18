33a57d7fab810aebf7a90d5863f552941477aa0a
Greg Maxey <gmaxey@cloudbees.com>
2022-11-18 14:25:07 +0000

    parameterization improvements

--- GetFullCredentialFromDSL.groovy
+++ GetFullCredentialFromDSL.groovy
@@ -5,18 +5,18 @@ CloudBees CDRO DSL: Extract credential value in a DSL step
 */
 
 procedure 'Get full credentials with DSL', {
-  projectName = 'Default'
+	projectName = 'Default'
 
-  formalParameter 'cred', defaultValue: '', {
-    required = '1'
-    type = 'credential'
-  }
+	formalParameter 'cred', defaultValue: '', {
+		required = '1'
+		type = 'credential'
+	}
 
-  step 'Get password', {
-    command = 'property "/myJob/cred", value: getFullCredential(credentialName:"cred", jobStepId: "$[/myJobStep/jobStepId]").password'
-    shell = 'ectool evalDsl --dslFile'
-    attachParameter {
-      formalParameterName = '/projects/Default/procedures/Get full credentials with DSL/formalParameters/cred'
-    }
-  }
+	step 'Get password', {
+		command = 'property "/myJob/cred", value: getFullCredential(credentialName:"cred", jobStepId: "$[/myJobStep/jobStepId]").password'
+		shell = 'ectool evalDsl --dslFile'
+		attachParameter {
+			formalParameterName = "/projects/${projectName}/procedures/${procedureName}/formalParameters/cred"
+		}
+	}
 }
