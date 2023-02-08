644f07697e2aa837a268666fd4c24f63c45ab52f
Greg Maxey <gmaxey@cloudbees.com>
2023-02-08 21:50:05 +0000

    Output tab delimited

--- FindOldPluginConfigurations.groovy
+++ FindOldPluginConfigurations.groovy
@@ -2,13 +2,13 @@
 
 Find old plugin configurations
 Run this DSL in the DSL IDE
+Out put is a tab-delimited list of plugin names and configuration names; this can be cut and pasted to a spreadsheet
 
 
 */
 
 
-//def proj = [projectName: "BiogenTester-1.0.0.0",pluginKey: "BiogenTester"]
-def confs = []
+def confs=""
 getProjects().each { proj ->
   if (proj.pluginKey) {
     def ec_config = getProperty(projectName: proj.projectName, propertyName: "ec_config")?true:false
@@ -16,7 +16,7 @@ getProjects().each { proj ->
       def configLocation = getProperty(projectName: proj.projectName, propertyName: "ec_config/configLocation")?.value
       if (configLocation) if (configLocation && getProperty(projectName: proj.projectName, propertyName: configLocation)) {
         getProperties(projectName: proj.projectName, propertyName: configLocation).property.each { conf ->
-          confs.add([(proj.pluginKey):conf.name])
+          confs += proj.pluginKey + '\t' + conf.name + '\n'
         }
       }
     }
