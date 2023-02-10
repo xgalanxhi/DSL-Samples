721f2a2dd7a743cdd6b92954f60042ec18aef20d
Greg Maxey <gmaxey@cloudbees.com>
2023-02-10 15:16:04 +0000

    Add support designation and column headers

--- FindOldPluginConfigurations.groovy
+++ FindOldPluginConfigurations.groovy
@@ -8,15 +8,16 @@ Out put is a tab-delimited list of plugin names and configuration names; this ca
 */
 
 
-def confs=""
+def confs='Configuration\tPlugin\tSupport\n'
 getProjects().each { proj ->
   if (proj.pluginKey) {
     def ec_config = getProperty(projectName: proj.projectName, propertyName: "ec_config")?true:false
     if (ec_config) {
       def configLocation = getProperty(projectName: proj.projectName, propertyName: "ec_config/configLocation")?.value
       if (configLocation) if (configLocation && getProperty(projectName: proj.projectName, propertyName: configLocation)) {
+        def plug = getPlugin(pluginName: proj.pluginKey)
         getProperties(projectName: proj.projectName, propertyName: configLocation).property.each { conf ->
-          confs += proj.pluginKey + '\t' + conf.name + '\n'
+          confs +=  conf.name + '\t' + proj.pluginKey + '\t'  +  plug.supportedBy + '\n'
         }
       }
     }
