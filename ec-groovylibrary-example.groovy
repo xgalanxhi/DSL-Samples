45c97a7576ae5d35d8c360c329262c2c5f61a1ae
holywen <shaohua.wen@gmail.com>
2023-05-22 16:16:58 +0700

    add two lib

--- ec-groovylibrary-example.groovy
+++ ec-groovylibrary-example.groovy
@@ -8,7 +8,7 @@ procedure 'groovylib-helloworld', {
     timeLimit = '0'
     timeLimitUnits = 'seconds'
     actualParameter 'config', '/projects/HolyPlayground/pluginConfigurations/git-config'
-    actualParameter 'gitRepoFolder', '/tmp/groovy-cd-lib'
+    actualParameter 'gitRepoFolder', ''
     actualParameter 'mirror', 'false'
     actualParameter 'overwrite', 'false'
     actualParameter 'repoUrl', 'https://github.com/holywen-cd/groovy-cd-lib'
@@ -17,14 +17,38 @@ procedure 'groovylib-helloworld', {
     actualParameter 'submodules', 'false'
   }
 
+  step 'getLibSource2', {
+    subprocedure = 'Clone'
+    subproject = '/plugins/EC-Git/project'
+    timeLimit = '0'
+    timeLimitUnits = 'seconds'
+    actualParameter 'branch', ''
+    actualParameter 'commit', ''
+    actualParameter 'config', '/projects/HolyPlayground/pluginConfigurations/git-config'
+    actualParameter 'depth', ''
+    actualParameter 'gitRepoFolder', ''
+    actualParameter 'mirror', 'false'
+    actualParameter 'overwrite', 'false'
+    actualParameter 'pathspecs', ''
+    actualParameter 'referenceFolder', ''
+    actualParameter 'repoUrl', 'https://github.com/holywen-cd/groovy-cd-lib2'
+    actualParameter 'resultPropertySheet', '/myJob/clone'
+    actualParameter 'shallowSubmodules', 'false'
+    actualParameter 'submodules', 'false'
+    actualParameter 'tag', ''
+  }
+
   step 'useLib', {
     command = '''import org.holywen.Example
+import org.holywen.Example1
 import com.electriccloud.client.groovy.ElectricFlow
 
 ElectricFlow ef = new ElectricFlow()
 def example = new Example()
-example.helloWorld()'''
-    shell = 'ec-groovy -cp /tmp/groovy-cd-lib/src'
+example.helloWorld()
+def example1 = new Example1()
+example1.helloWorld()'''
+    shell = 'ec-groovy -cp groovy-cd-lib/src:groovy-cd-lib2/src'
     timeLimit = '0'
     timeLimitUnits = 'seconds'
   }
