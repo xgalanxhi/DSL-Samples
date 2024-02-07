56bf36acafc019f5b0181c45897587f36397b253
Rick Broker <rbroker@cloudbees.com>
2024-02-07 13:33:33 -0500

    Add GetJsonFromJenkinsJob

---
+++ GetJsonFromJenkinsJob/GetJsonFromJenkinsJob.groovy
@@ -0,0 +1,93 @@
+*
+ Copyright 2023 Cloudbees
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+---------------------------------------------------------------------------------
+
+
+ElectricFlow DSL Example: Print out JSON equivalent of property sheet
+
+Instructions:
+1. Run this DSL code from the command line, DSL Editor or DSLIDE
+		Command line: ectool evalDsl --dslFile prop2json.groovy
+2. Run the procedure against the sample property sheet (default parameter value)
+	or other property sheet. Make sure to reference the property using XPATH, e.g.,
+	/projects/My Project/pipelines/My Pipeline/stages/First Stage/myPropertySheetName
+
+*/
+
+def CurrentProject = 'dslsamples'
+
+procedure 'GetJsonFromJenkinsJob', {
+  projectName =  CurrentProject,
+  timeLimit = '0'
+
+  formalParameter 'JenkinsConfig', {
+    orderIndex = '1'
+    propertyReference = 'EC-Jenkins'
+    required = '1'
+    type = 'pluginConfiguration'
+  }
+
+  formalParameter 'JenkinsJob', defaultValue: 'TestJob1', {
+    orderIndex = '2'
+    required = '1'
+    type = 'entry'
+  }
+
+  formalParameter 'JenkinsArtifact', defaultValue: 'test.json', {
+    orderIndex = '3'
+    required = '1'
+    type = 'entry'
+  }
+
+  step 'Get Artifact From Jenkins', {
+    actualParameter = [
+      'artifacts': '$[JenkinsArtifact]',
+      'build_number': '',
+      'config_name': '$[JenkinsConfig]',
+      'job_name': '$[JenkinsJob]',
+      'target_directory': '',
+    ]
+    procedureName = 'GetJsonFromJenkinsJob'
+    subprocedure = 'DownloadArtifacts'
+    subproject = '/plugins/EC-Jenkins/project'
+    timeLimit = '0'
+    timeLimitUnits = 'seconds'
+  }
+
+  step 'Get JSON Data', {
+    command = '''import com.electriccloud.client.groovy.ElectricFlow
+import com.electriccloud.client.groovy.models.*
+
+
+ElectricFlow ef = new ElectricFlow()
+
+File file = new File("$[JenkinsArtifact]")
+def String jsonArtifact = file.getText()
+println( jsonArtifact )
+ef.setProperty(
+                propertyName: \'/myJob/jsonText\',
+                value: jsonArtifact )'''
+    procedureName = 'GetJsonFromJenkinsJob'
+    shell = 'ec-groovy'
+    timeLimit = '0'
+    timeLimitUnits = 'seconds'
+  }
+
+  step 'json2properties', {
+    actualParameter = [
+      'json': '$[/myJob/jsonText]',
+      'startingpath': '/myJob/expandedJson',
+    ]
+    procedureName = 'GetJsonFromJenkinsJob'
+    subprocedure = 'json2properties'
+    timeLimit = '0'
+    timeLimitUnits = 'seconds'
+  }
+}
---
+++ GetJsonFromJenkinsJob/README.md
@@ -0,0 +1,51 @@
+# json2properties & properties2json
+
+## Description
+
+This procedure gets a JSON file that has been marked as an artifact in a Jenkins/Cloudbees CI server, convert the JSON into a property set and make it available to other tasks in CDRO.  This procedure uses the json2properties procedures to process at json file collected from the Jenkins/Cloudbees CI server and the Jenkins plugin.  You will also need to configure a connection to your Jenkins server or Cloudbees CI server as a Jenkins server.
+
+
+### Installing
+
+#### Installing json2properties
+
+* Edit `GetJsonFromJenkinsJob.groovy` and change `CurrentProject` to the project you want to install to
+
+```
+ef CurrentProject = 'dslsamples'
+
+procedure 'GetJsonFromJenkinsJob', {
+  projectName =  CurrentProject,
+  timeLimit = '0'
+
+...
+...
+...
+```
+
+* Run the DSL `GetJsonFromJenkinsJob.groovy` to install
+
+#### Installing properties2json
+
+* Edit `GetJsonFromJenkinsJob.groovy` and change `CurrentProject` to the project you want to install to
+
+```
+def CurrentProject = 'dslsamples'
+
+project CurrentProject,{
+	// Sample property sheet
+	property "expandedJson",{
+...
+...
+...
+```
+
+* Run the DSL `GetJsonFromJenkinsJob.groovy` to install
+
+## Example
+
+#### Running json2properties
+
+The first step here will be to use the [GetJsonFromJenkinsJob](GetJsonFromJenkinsJob.groovy) procedure to create a property sheet in  the default project.  Running `GetJsonFromJenkinsJob`  looks like the following:
+
+![RunProcedure_json2properties](img/GetJsonFromJenkinsStart.png)
